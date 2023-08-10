06f409fe9fee1c8375d7c4fff6a82e74dac9aca5
DESKTOP-IM56609\dell <xiaojunfeng24@163.com>
2023-08-10 16:03:23 +0800

    spring-cloud 新增模块cloud-nacos

---
+++ cloud-nacos/src/main/java/com/springboot/cloud/NacosApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosApplication.class, args);
+    }
+
+}
